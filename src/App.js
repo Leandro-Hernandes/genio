759bc9538bab84a573a0b2f29e363b278abfa499
Leandro <leandrohernandesm@gmail.com>
2024-10-25 17:47:38 -0300

    Alteração

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+
+//Import da estilização do componente principal
 import './App.css';
+//Import do componente Main
+import Main from './components/Main/Main';
+//Import da estilização do componente Main
+import './components/Main/Main.css';
 
-function App() {
+function App() { 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Main/>    
+    </>
+    
   );
-}
 
+}
 export default App;
